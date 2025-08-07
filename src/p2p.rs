855dcb82ed576ef6590fdc4b57698ae2a2e0a8d3
Guyed Riley <guybriley02@gmail.com>
2025-08-06 21:56:49 -0400

    Update p2p.rs

--- src/p2p.rs
+++ src/p2p.rs
@@ -1,44 +1,15 @@
-use libp2p::{
-    gossipsub::{self, IdentTopic as Topic},
-    mdns::tokio::Behaviour as Mdns,
-    swarm::NetworkBehaviour,
-    PeerId,
-};
-use serde::{Deserialize, Serialize};
-use crate::block::Block;
+// ... imports ...
+use crate::block::{Block, Transaction}; // Add Transaction
 
 // The messages we'll send across the network.
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ChainMessage {
-    Block(Block), // Announce a new block to the network
+    Block(Block),
+    Transaction(Transaction), // New variant for gossiping transactions
 }
 
-// The topic for our gossipsub protocol. All nodes must subscribe to the same topic.
+// Topics for our gossipsub protocol
 pub const CHAIN_TOPIC: Topic = Topic::new("kosher-chain-blocks");
+pub const TRANSACTION_TOPIC: Topic = Topic::new("kosher-chain-transactions");
 
-// This is the main network behaviour struct that combines all P2P protocols.
-#[derive(NetworkBehaviour)]
-#[behaviour(to_swarm = "ChainBehaviourEvent")]
-pub struct ChainBehaviour {
-    pub gossipsub: gossipsub::Behaviour,
-    pub mdns: Mdns,
-}
-
-// Events that our ChainBehaviour can produce.
-#[derive(Debug)]
-pub enum ChainBehaviourEvent {
-    Gossipsub(gossipsub::Event),
-    Mdns(libp2p::mdns::Event),
-}
-
-impl From<gossipsub::Event> for ChainBehaviourEvent {
-    fn from(event: gossipsub::Event) -> Self {
-        ChainBehaviourEvent::Gossipsub(event)
-    }
-}
-
-impl From<libp2p::mdns::Event> for ChainBehaviourEvent {
-    fn from(event: libp2p::mdns::Event) -> Self {
-        ChainBehaviourEvent::Mdns(event)
-    }
-}
+// ... rest of the file remains the same ...
